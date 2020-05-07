40287672214cb46cd93827f7fb4cc91f17ed37a3
songxianzhuo <happysong8783@163.com>
2020-05-07 23:30:27 +0800

    spring security

--- security-demo/src/main/java/com/example/securitydemo/config/WebSecurityConfig.java
+++ security-demo/src/main/java/com/example/securitydemo/config/WebSecurityConfig.java
@@ -19,41 +19,57 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
-     * authorizeRequests() 定义哪些URL需要被保护、哪些不需要被保护
-     * formLogin() 定义当需要用户登录时候，转到的登录页面
+     * 请求授权
+     * spring security常用请求路径匹配器
+     *      -antMatchers：使用Ant风格的路径拦截，如"/login","/home";
+     *      -regexMatchers：使用正则表达式匹配路径
+     *  资料：springboot实战344页
      * @param http
      * @throws Exception
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                // 授权请求
             .authorizeRequests()
+                // 路径为"/"和"/home"的可以任意访问
                 .antMatchers("/", "/home").permitAll()
-                .anyRequest()
-                .authenticated()
+                // 其他请求必须经过身份认证
+                .anyRequest().authenticated()
                 .and()
+                // 定义用户登录行为
             .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                // 设置登录首页，默认是"/login",登录页面可以任意访问
+                .loginPage("/login").permitAll()
                 .and()
+                // 定义注销行为
             .logout()
-                .permitAll();
+                // 设置注销路径，默认“/logout”,注销页面可以任意访问
+                .logoutUrl("/logout").permitAll();
     }
 
     /**
-     * 内存中创建了一个用户，该用户的名称为user，密码为password，用户角色为USER
-     * 不加.passwordEncoder(new MyPasswordEncoder())，密码就不是以明文的方式进行匹配，会报错，java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
-     * 加了.passwordEncoder(new MyPasswordEncoder())，密码就是以明文的方式进行匹配
+     * 用户认证
+     * 三种方式，来源：springboot实战343页
+     *      -内存中添加用户
+     *      -jdbc添加用户
+     *      -实现UserDetailsService接口添加用户
+     * 另外外 也可以使用 jdbcAuthentication()方法添加数据库中的用户
      * @param auth
      * @throws Exception
      */
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-            .inMemoryAuthentication()
+                // 在内存中添加用户，用户的名称为user，密码为password，用户角色为USER
+                .inMemoryAuthentication()
+                // 指定密码编码器，不配报错，默认是不是以明文进行密码匹配，添加后就可以以明文方式进行匹配
                 .passwordEncoder(new MyPasswordEncoder())
+                // 配置用户user
                 .withUser("user")
+                // 配置用户user密码
                 .password("password")
+                // 配置用户user角色
                 .roles("USER");
     }
 }
